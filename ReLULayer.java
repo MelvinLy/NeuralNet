6d48eaf27229022c45f4932887951e6a25bae0bd
melvinly <melvinly@gmail.com>
2020-02-29 23:24:32 -0500

    no message

--- ReLULayer.java
+++ ReLULayer.java
@@ -1,9 +1,25 @@
 
 class ReLULayer extends Layer {
 
-	double[] getOutputs() {
-
-		return null;
+	ReLULayer() {
+		super();
 	}
-
+	
+	double[] getOutputs(double[] input) {
+		if(input.length != this.size()) {
+			return null;
+		}
+		Node[] nodes = this.getNodes();
+		double[] toReturn = new double[nodes.length];
+		for(int a = 0; a < nodes.length; a++) {
+			Node currentNode = nodes[a];
+			for(int b = 0; b < currentNode.getNumOuts(); b++) {
+				toReturn[a] = toReturn[a] + input[a] * currentNode.getMultipliers()[b];
+			}
+		}
+		for(int a = 0; a < toReturn.length; a++) {
+			toReturn[a] = reLU(toReturn[a]);
+		}
+		return toReturn;
+	}
 }
