2a09a3d6c805c341f10004d557919e73c6e443a5
melvinly <melvinly@gmail.com>
2020-03-01 18:24:52 -0500

    Interface for custom layers.

---
+++ TestSigmoidLayer.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class TestSigmoidLayer extends SigmoidLayer {
+
+	TestSigmoidLayer() {
+		super();
+	}
+	
+	public double[] modifies(double[] in) {
+		for(int a = 0; a < in.length; a++) {
+			in[a] = in[a] - 2;
+		}
+		return in;
+	}
+}
