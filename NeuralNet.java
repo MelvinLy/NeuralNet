d3e1ebc318d8c49eb99838edb11e9e1e538f35b2
melvinly <melvinly@gmail.com>
2020-02-29 23:46:24 -0500

    no message

--- NeuralNet.java
+++ NeuralNet.java
@@ -1,13 +1,25 @@
 
 public class NeuralNet {
 	private Layer inputLayer;
+	private Layer outputLayer;
 
 	public NeuralNet() {
 		this.inputLayer = null;
+		this.outputLayer = null;
 	}
 	
 	public NeuralNet(Layer inputLayer) {
 		this.inputLayer = inputLayer;
+		this.outputLayer = inputLayer;
+	}
+	
+	public boolean addLayer(Layer layer) {
+		if(layer.size() != outputLayer.getNodes()[0].getNumOuts()) {
+			return false;
+		}
+		this.outputLayer.addNextLayer(layer);
+		this.outputLayer = layer;
+		return true;
 	}
 	
 	public double[] getOutput(double[] input) {
