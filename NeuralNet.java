b5e599331a2513c80f8766834b1da3c0acdcc9ae
melvinly <melvinly@gmail.com>
2020-02-29 23:30:23 -0500

    no message

--- NeuralNet.java
+++ NeuralNet.java
@@ -1,5 +1,14 @@
 
 public class NeuralNet {
+	private Layer inputLayer;
 
-
+	public NeuralNet() {
+		this.inputLayer = null;
+	}
+	
+	public NeuralNet(Layer inputLayer) {
+		this.inputLayer = inputLayer;
+	}
+	
+	
 }
