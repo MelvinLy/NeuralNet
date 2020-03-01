77d7360f5a971a5e4039b349f9c8af5ac4730a16
melvinly <melvinly@gmail.com>
2020-03-01 12:52:11 -0500

    no message

--- Main.java
+++ Main.java
@@ -3,22 +3,62 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) throws LayerMismatchException, EmptyLayerException, NodeSizeMismatchException {
 		NeuralNet test = new NeuralNet();
-		
+
 		double[] in = {0.1, 1, 0.2, 0.45, -1};
 		
 		Layer input = NeuralNet.createLayer(1);
-		for(int a = 0; a < 5; a++) {
-			Node node = Layer.createNode(7);
+		int layerSize = 5;
+		int outputSize = 7;
+		for(int a = 0; a < layerSize; a++) {
+			Node node = Layer.createNode(outputSize);
 			for(int b = 0; b < node.getNumOuts(); b++) {
 				node.setMultiplier(b, 0.5);
 			}
 			input.addNode(node);
 		}
 		test.addLayer(input);
-/////////////////////////////////////////////////////////////////
+		/////////////////////////////////////////////////////////////////
 		Layer layer = NeuralNet.createLayer(1);
-		for(int a = 0; a < 7; a++) {
-			Node node = Layer.createNode(7);
+		layerSize = 7;
+		outputSize = 2;
+		for(int a = 0; a < layerSize; a++) {
+			Node node = Layer.createNode(outputSize);
+			for(int b = 0; b < node.getNumOuts(); b++) {
+				node.setMultiplier(b, 0.5);
+			}
+			layer.addNode(node);
+		}
+		test.addLayer(layer);
+		/////////////////////////////////////////////////////////////////
+		layer = NeuralNet.createLayer(1);
+		layerSize = 2;
+		outputSize = 10;
+		for(int a = 0; a < layerSize; a++) {
+			Node node = Layer.createNode(outputSize);
+			for(int b = 0; b < node.getNumOuts(); b++) {
+				node.setMultiplier(b, 0.5);
+			}
+			layer.addNode(node);
+		}
+		test.addLayer(layer);
+		/////////////////////////////////////////////////////////////////
+		layer = NeuralNet.createLayer(1);
+		layerSize = 10;
+		outputSize = 5;
+		for(int a = 0; a < layerSize; a++) {
+			Node node = Layer.createNode(outputSize);
+			for(int b = 0; b < node.getNumOuts(); b++) {
+				node.setMultiplier(b, 0.5);
+			}
+			layer.addNode(node);
+		}
+		test.addLayer(layer);
+		/////////////////////////////////////////////////////////////////
+		layer = NeuralNet.createLayer(1);
+		layerSize = 5;
+		outputSize = 0;
+		for(int a = 0; a < layerSize; a++) {
+			Node node = Layer.createNode(outputSize);
 			for(int b = 0; b < node.getNumOuts(); b++) {
 				node.setMultiplier(b, 0.5);
 			}
@@ -26,7 +66,8 @@ public class Main {
 		}
 		test.addLayer(layer);
 
-		
+
+
 		double[] out = test.getOutput(in);		
 		System.out.println(Arrays.toString(out));
 		System.out.println(test.size());
