9e269f6270c79022158c30527917e91100baaca6
MelvinLy <lymelvin@ymail.com>
2020-03-01 02:35:36 +0000

    Add files via upload

---
+++ NeuralNet.java
@@ -0,0 +1,23 @@
+
+
+public class NeuralNet {
+	
+	public double sigmoid(float x, float k) {
+		double nom = Math.E;
+		return ;
+	}
+	
+}
+
+class Node {
+	private Node parent;
+	private Node child;
+	private double multiplier;
+	
+	Node(Node parent, Node child, double multiplier) {
+		this.parent = parent;
+		this.child = child;
+		this.multiplier = multiplier;
+	}
+	
+}
