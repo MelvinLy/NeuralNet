cf7885a26bdafb18f686bc1a443ffc3c44f44183
MelvinLy <melvinly@gmail.com>
2020-06-04 19:21:46 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,12 +2,15 @@ import java.util.Arrays;
 
 public class Main {
 	
-	public static void print(Object a) {
+	public static void println(Object a) {
 		System.out.println(a);
 	}
 	
 	public static void main(String args[]) throws LayerSizeMismatchException, NullNodeException, NodeSizeMismatchException {
 		
+		
+		
+		/*
 		SigmoidLayer layer = new SigmoidLayer(5, 3);
 		SigmoidLayer layer2 = new SigmoidLayer(3, 3);
 		
@@ -25,7 +28,8 @@ public class Main {
 		double[] output = net.getOutput(input);
 		double[] expected = {0.9525740853634935, 0.9525740853634935, 0.9525740853634935};
 		
-		print(Arrays.toString(output) + "\n");
-		print(net.getNewWeight(1, 0, 0, input, expected, 0.1));
+		println(Arrays.toString(output) + "\n");
+		println(net.getNewWeight(1, 0, 0, input, expected, 0.1));
+		*/
 	}
 }
