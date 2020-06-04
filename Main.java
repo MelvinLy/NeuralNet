8e098c5899a567d0da1d94c7a986c4de3eb5e0da
MelvinLy <melvinly@gmail.com>
2020-06-04 19:44:33 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -15,33 +15,42 @@ public class Main {
 		
 		BufferedImage img = null;
 		try {
-			img = ImageIO.read(new File("one.png"));
+			img = ImageIO.read(new File("one.jpg"));
 		}
 		catch(IOException e) {
 			
 		}
-		byte[] pixels = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
-		println(Arrays.toString(pixels));
-		/*
-		SigmoidLayer layer = new SigmoidLayer(5, 3);
-		SigmoidLayer layer2 = new SigmoidLayer(3, 3);
-		
-		for(int a = 0; a < layer.size(); a++) {
-			layer.setNode(a, new Node(3));
+		int[][] pixels = new int[32][32];
+		for(int a = 0; a < 32; a++) {
+			for(int b = 0; b < 32; b++) {
+				pixels[a][b] = img.getRGB(a, b);
+			}
 		}
 		
-		for(int a = 0; a < layer2.size(); a++) {
-			layer2.setNode(a, new Node(3));
-		}
 		
-		NeuralNetwork net = new NeuralNetwork(layer, layer2);
-		
-		double[] input = {1,2,3,4,5};
-		double[] output = net.getOutput(input);
-		double[] expected = {0.9525740853634935, 0.9525740853634935, 0.9525740853634935};
-		
-		println(Arrays.toString(output) + "\n");
-		println(net.getNewWeight(1, 0, 0, input, expected, 0.1));
-		*/
+
 	}
 }
+
+
+/* Paste in main
+SigmoidLayer layer = new SigmoidLayer(5, 3);
+SigmoidLayer layer2 = new SigmoidLayer(3, 3);
+
+for(int a = 0; a < layer.size(); a++) {
+	layer.setNode(a, new Node(3));
+}
+
+for(int a = 0; a < layer2.size(); a++) {
+	layer2.setNode(a, new Node(3));
+}
+
+NeuralNetwork net = new NeuralNetwork(layer, layer2);
+
+double[] input = {1,2,3,4,5};
+double[] output = net.getOutput(input);
+double[] expected = {0.9525740853634935, 0.9525740853634935, 0.9525740853634935};
+
+println(Arrays.toString(output) + "\n");
+println(net.getNewWeight(1, 0, 0, input, expected, 0.1));
+*/
