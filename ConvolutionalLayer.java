adfc9c0c75ff3d3bbf3c9aaf2c2b93bbd87076b3
MelvinLy <melvinly@gmail.com>
2020-06-04 20:32:53 -0400

    Create ConvolutionalLayer.java

---
+++ ConvolutionalLayer.java
@@ -0,0 +1,21 @@
+
+public class ConvolutionalLayer extends Layer {
+
+	//Some methods may not be compatible with other layers.
+	
+	public ConvolutionalLayer(int size, int outputSize) {
+		super(size, outputSize);
+		// TODO Auto-generated constructor stub
+	}
+
+	public double dCostbyDWeight(int node, int edge, double[] input, double[] expected) throws NullNodeException {
+
+		return 0;
+	}
+
+	public double[] activate(double[] input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
