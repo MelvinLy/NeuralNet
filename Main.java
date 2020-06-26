1a31c9f991d1b47d3201d536dd1417f7b5be5249
MelvinLy <melvinly@gmail.com>
2020-06-26 17:38:23 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,19 +13,19 @@ public class Main {
 		double[] input = {1, 2, 3, 4, 5};
 		double[] out = test.getRawOutput(input);
 		
-		SigmoidLayer test2 = new SigmoidLayer(2, 2);
+		SigmoidLayer test2 = new SigmoidLayer(2, 3);
 		
 		NeuralNetwork network = new NeuralNetwork(test);
 		network.addLayer(test2);
 		
 		println(Arrays.toString(network.getOutput(input)));
 		double[][] allOutputs = network.getAllOutputs(input);
-		println(Arrays.deepToString(allOutputs));
+		//println(Arrays.deepToString(allOutputs));
 		
 		double[] raw = test2.getRawOutput(new double[]{0.9999996940977731, 0.9999996940977731});
 		double newWeight = test2.getNewWeight(0.8807970137423242, 1, test2.getWeight(0, 0), 0.01, raw[0]);
-		println(newWeight);
-		network.fit(input, new double[] {1,0}, 100000, 0.1);
+		//println(newWeight);
+		network.fit(input, new double[] {1,0,1}, 1000000, 0.1);
 		println(Arrays.toString(network.getOutput(input)));
 	}
 }
