0946558f8f648220e122cf41ed51c45e284af0ea
MelvinLy <melvinly@gmail.com>
2020-09-16 15:38:01 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,35 @@ import java.util.Arrays;
 
 public class Main {
 
+	private static void testTenToTwoNetwork(NeuralNetwork network) throws InputSizeMismatchException, NoLayersException {
+		double[] input = new double[] {1,1,1,1,1,0,0,0,0,0};
+		double[] expectedOutput = new double[] {1, 0};
+		testSingleTenToTwoNetwork(network, input, expectedOutput);
+
+		input = new double[] {1,0,0,0,0,1,1,1,0,1};
+		expectedOutput = new double[] {0, 1};
+		testSingleTenToTwoNetwork(network, input, expectedOutput);
+		
+		input = new double[] {1,1,0,1,1,0,0,0,0,1};
+		expectedOutput = new double[] {1, 0};
+		testSingleTenToTwoNetwork(network, input, expectedOutput);
+		
+		input = new double[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		expectedOutput = new double[] {0, 0};
+		testSingleTenToTwoNetwork(network, input, expectedOutput);
+		
+		input = new double[] {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+		expectedOutput = new double[] {1, 1};
+		testSingleTenToTwoNetwork(network, input, expectedOutput);
+	}
+	
+	private static void testSingleTenToTwoNetwork(NeuralNetwork network, double[] input, double[] expectedOutput) throws InputSizeMismatchException, NoLayersException {
+		double[] predictedOutput = network.getOutputVector(input);
+		System.out.printf("Predicted: [%f, %f]\n", predictedOutput[0], predictedOutput[1]);
+		System.out.println("Expected: " + Arrays.toString(expectedOutput));
+		System.out.printf("Cost: %f\n\n", network.getCost(predictedOutput, expectedOutput));
+	}
+	
 	public static void runSimpleCase() throws InputSizeMismatchException, OutputSizeMismatchException, LayerSizeMismatchException, FileNotFoundException, IOException, NoLayersException {
 		final int LEARNING_CYCLES = 1000000;
 		final double LEARNING_RATE = 0.1;
@@ -11,44 +40,11 @@ public class Main {
 		NeuralNetwork network = new NeuralNetwork(new SigmoidLayer(10, 5));
 		network.addLayer(new SigmoidLayer(5, 3));
 		network.addLayer(new SigmoidLayer(3, 2));
-		double[] input = new double[] {1,1,1,1,1,0,0,0,0,0};
-		double[] expectedOutput = new double[] {1, 0};
-		double[] predictedOutput = network.getOutputVector(input);
-		System.out.printf("Predicted: [%f, %f]\n", predictedOutput[0], predictedOutput[1]);
-		System.out.println("Expected: " + Arrays.toString(expectedOutput));
-		System.out.printf("Cost: %f\n\n", network.getCost(predictedOutput, expectedOutput));
-		
-		input = new double[] {1,0,0,0,0,1,1,1,0,1};
-		expectedOutput = new double[] {0, 1};
-		predictedOutput = network.getOutputVector(input);
-		System.out.printf("Predicted: [%f, %f]\n", predictedOutput[0], predictedOutput[1]);
-		System.out.println("Expected: " + Arrays.toString(expectedOutput));
-		System.out.printf("Cost: %f\n\n", network.getCost(predictedOutput, expectedOutput));
-		
-		input = new double[] {1,1,0,1,1,0,0,0,0,1};
-		expectedOutput = new double[] {1, 0};
-		predictedOutput = network.getOutputVector(input);
-		System.out.printf("Predicted: [%f, %f]\n", predictedOutput[0], predictedOutput[1]);
-		System.out.println("Expected: " + Arrays.toString(expectedOutput));
-		System.out.printf("Cost: %f\n\n", network.getCost(predictedOutput, expectedOutput));
-		
-		input = new double[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		expectedOutput = new double[] {0, 0};
-		predictedOutput = network.getOutputVector(input);
-		System.out.printf("Predicted: [%f, %f]\n", predictedOutput[0], predictedOutput[1]);
-		System.out.println("Expected: " + Arrays.toString(expectedOutput));
-		System.out.printf("Cost: %f\n\n", network.getCost(predictedOutput, expectedOutput));
-		
-		input = new double[] {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-		expectedOutput = new double[] {1, 1};
-		predictedOutput = network.getOutputVector(input);
-		System.out.printf("Predicted: [%f, %f]\n", predictedOutput[0], predictedOutput[1]);
-		System.out.println("Expected: " + Arrays.toString(expectedOutput));
-		System.out.printf("Cost: %f\n\n", network.getCost(predictedOutput, expectedOutput));
+		testTenToTwoNetwork(network);
 		
 		System.out.println("--------------------------");
 		System.out.println("Training...");
-		System.out.println("--------------------------\n");
+		System.out.println("--------------------------");
 		long start = System.currentTimeMillis();
 		network.fit(
 			new double[][] {
@@ -71,49 +67,11 @@ public class Main {
 				{0, 0},
 				{1, 1},
 			}, 
-			LEARNING_CYCLES, LEARNING_RATE);
-		long end = System.currentTimeMillis();
-		
-		input = new double[] {1,1,1,1,1,0,0,0,0,0};
-		expectedOutput = new double[] {1, 0};
-		predictedOutput = network.getOutputVector(input);
-		System.out.printf("Predicted: [%f, %f]\n", predictedOutput[0], predictedOutput[1]);
-		System.out.println("Expected: " + Arrays.toString(expectedOutput));
-		System.out.printf("Cost: %f\n\n", network.getCost(predictedOutput, expectedOutput));
-		
-		input = new double[] {1,0,0,0,0,1,1,1,0,1};
-		expectedOutput = new double[] {0, 1};
-		predictedOutput = network.getOutputVector(input);
-		System.out.printf("Predicted: [%f, %f]\n", predictedOutput[0], predictedOutput[1]);
-		System.out.println("Expected: " + Arrays.toString(expectedOutput));
-		System.out.printf("Cost: %f\n\n", network.getCost(predictedOutput, expectedOutput));
-		
-		input = new double[] {1,1,0,1,1,0,0,0,0,1};
-		expectedOutput = new double[] {1, 0};
-		predictedOutput = network.getOutputVector(input);
-		System.out.printf("Predicted: [%f, %f]\n", predictedOutput[0], predictedOutput[1]);
-		System.out.println("Expected: " + Arrays.toString(expectedOutput));
-		System.out.printf("Cost: %f\n\n", network.getCost(predictedOutput, expectedOutput));
-		
-		input = new double[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		expectedOutput = new double[] {0, 0};
-		predictedOutput = network.getOutputVector(input);
-		System.out.printf("Predicted: [%f, %f]\n", predictedOutput[0], predictedOutput[1]);
-		System.out.println("Expected: " + Arrays.toString(expectedOutput));
-		System.out.printf("Cost: %f\n\n", network.getCost(predictedOutput, expectedOutput));
-		
-		input = new double[] {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-		expectedOutput = new double[] {1, 1};
-		predictedOutput = network.getOutputVector(input);
-		System.out.printf("Predicted: [%f, %f]\n", predictedOutput[0], predictedOutput[1]);
-		System.out.println("Expected: " + Arrays.toString(expectedOutput));
-		System.out.printf("Cost: %f\n\n", network.getCost(predictedOutput, expectedOutput));
-		
-		System.out.println("--------------------------");
-		System.out.printf("Training took: %.2f s\n", (float) (end - start) / 1000);
-		System.out.println("--------------------------");
-		
-		//network.saveNeuralNetwork("simple.txt");
+			LEARNING_CYCLES, LEARNING_RATE
+		);
+		System.out.printf("Training took: %.2fs\n\n" , (System.currentTimeMillis() - start) / 1000.0);
+
+		testTenToTwoNetwork(network);
 	}
 	
 	public static void main(String[] args) throws InputSizeMismatchException, LayerSizeMismatchException, OutputSizeMismatchException, IOException, ClassNotFoundException, NoLayersException {
