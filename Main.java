5e621b8c1cf7a1295b5c5afd4b05ef183db4f7e4
MelvinLy <melvinly@gmail.com>
2020-09-26 18:53:12 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 public class Main {
 	
 	public static void main(String[] args) throws InputSizeMismatchException, LayerSizeMismatchException, OutputSizeMismatchException, IOException, ClassNotFoundException, NoLayersException {
+		//Uncomment to run the simple test case.
 		//Example.runSimpleCase();
 	}
 }
